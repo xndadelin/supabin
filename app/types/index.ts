4920d702413987129daee1e0399a93f3fff7d082
xndadelin <adelinmihai071@gmail.com>
2025-11-27 20:10:39 +0200

    Add types.

---
+++ app/types/index.ts
@@ -0,0 +1,34 @@
+export interface FileData {
+    id: string;
+    name: string;
+    size: number;
+    type: string;
+    progress?: number;
+    status?: 'pending' | 'uploading' | 'completed'
+}
+
+export interface UploadData {
+    id: string;
+    name: string;
+    hasPassword: boolean;
+    password?: string;
+    expiryTime: string;
+    maxDownloads: string;
+    files: FileData[];
+}
+
+export interface SelectOption {
+    value: string;
+    label: string;
+}
+
+export interface UploadSettings {
+    uploadName: string;
+    password: string;
+    email: string;
+    expiryTime: string;
+    maxDownloads: string;
+    allowPreview: boolean;
+    customSlug: string;
+}
+
